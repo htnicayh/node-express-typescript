04b0ac652eced465aba28ce39df6cb93b67688ac
htnicayh <hieuhp1306@gmail.com>
2022-01-23 15:59:20 +0700

    test lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,21 @@
-import express, { Response } from 'express';
+import express, { Response } from 'express'
 
 async function bootstrap() {
-    const app: express.Application = express();
+    const app: express.Application = express()
 
     app.get('/', (_: unknown, res: Response) => {
-        res.send('Hello');
-    });
+        res.send('Hello')
+    })
 
     app.listen(3000, () => {
-        console.log('Server is running at http://localhost:3000');
-    });
+        console.log('Server is running at http://localhost:3000')
+    })
 }
 
 (async () => {
     try {
-        await bootstrap();
+        await bootstrap()
     } catch (e) {
-        console.log(e.message);
+        console.log(e.message)
     }
-})();
+})()
