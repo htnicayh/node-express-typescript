be5443e1fa8a394c03066c0c0a0396fc18e51b4c
htnicayh <hieuhp1306@gmail.com>
2022-01-23 15:00:55 +0700

    create node typescript

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Response } from 'express'
+
+async function bootstrap() {
+    const app: express.Application = express()
+
+    app.get('/', (_: unknown, res: Response) => {
+        res.send('Hello')
+    })
+
+    app.listen(3000, () => {
+        console.log('Server is running at http://localhost:3000')
+    })
+}
+
+(async () => {
+    try {
+        await bootstrap()
+    } catch (e) {
+        console.log(e.message)
+    }
+})()
